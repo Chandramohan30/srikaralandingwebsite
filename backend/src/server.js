565f6cad6a77f05617b89320913748b9dd04e90c
chandramohan <chankannan30@gmail.com>
2026-09-03 14:44:01 +0530

    changes for deployed url

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const app = require("./app");
 
 const PORT = process.env.PORT || 5000;
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = "mongodb+srv://chankannan30_db_user:chan30%40dec@cluster0.ax74v02.mongodb.net/srikara?appName=Cluster0"
 
 if (!MONGODB_URI) {
   console.error("MONGODB_URI is not set. Copy .env.example to .env and add your Atlas connection string.");
